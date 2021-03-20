2b730d8141cdb6cb15c6a9a36bcdfd86edd80545
edmundsj <jordan.e@berkeley.edu>
2021-03-20 09:42:07 -0700

    updated init, source files, test files. All tests passing locally

--- sugarplot/__init__.py
+++ sugarplot/__init__.py
@@ -1,2 +1,7 @@
-from sugarplot.source.some_module import SomeClass
+import matplotlib.pyplot as plt
+# Change global settings of all plots generated in the future
+
+from sugarplot.source.prettify import *
+from sugarplot.source.normalization import *
+from sugarplot.source.plotters import *
 from sugarplot.test.shorthand import *
